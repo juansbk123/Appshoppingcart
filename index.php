43c94b9702cd4d82e1ecdfa791051f17a9813cc5
juan cruz <juansbk.darkdemon@gmail.com>
2021-02-21 03:56:34 -0500

    a greeting

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-$url = !empty($_GET['url']) ? ($_GET['url']) : 'home';
-$arrUrl = explode("/", $url);
-$controller = $arrUrl[0];
-$controllerFile = "views/".$controller.".php";
-if(file_exists($controllerFile)) {
-    require_once($controllerFile);
-}
+
+echo "Hla juan ";
+// $url = !empty($_GET['url']) ? ($_GET['url']) : 'home';
+// $arrUrl = explode("/", $url);
+// $controller = $arrUrl[0];
+// $controllerFile = "views/".$controller.".php";
+// if(file_exists($controllerFile)) {
+//     require_once($controllerFile);
+// }
 ?>
