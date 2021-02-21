c853565ac28805c28398bd8928894ba164568a61
juan cruz <juansbk.darkdemon@gmail.com>
2021-02-21 04:00:29 -0500

    redirect home

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 
-echo "Hla juan ";
-// $url = !empty($_GET['url']) ? ($_GET['url']) : 'home';
-// $arrUrl = explode("/", $url);
-// $controller = $arrUrl[0];
-// $controllerFile = "views/".$controller.".php";
-// if(file_exists($controllerFile)) {
-//     require_once($controllerFile);
-// }
+header("Lacation:admin/home.php");
 ?>
