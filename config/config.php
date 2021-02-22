7ef0ab10bd593f09a4855263ef28a56546d624f2
juan cruz <juansbk.darkdemon@gmail.com>
2021-02-22 06:40:15 -0500

    PROBADNDO HOST

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@
 
 return array(
     "driver"    =>"mysql",
-    "host"      =>"https://appshoppingcart.azurewebsites.net",
+    "host"      =>"DD6E0ECF1FE4E0D7F1E5CF4B4EF71629FA34500D7A4338D7A3676F039EA755E2",
     "user"      =>"juansbkadminroot",
     "pass"      =>"Jzxc123asd456",
     "database"  =>"shopping_cart",
