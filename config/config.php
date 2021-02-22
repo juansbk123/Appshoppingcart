eb70225d4f6601f0e2fb380ec72ff4a3cbe5e14e
juan cruz <juansbk.darkdemon@gmail.com>
2021-02-22 06:43:09 -0500

    PROBADNDO HOST_1

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@
 
 return array(
     "driver"    =>"mysql",
-    "host"      =>"DD6E0ECF1FE4E0D7F1E5CF4B4EF71629FA34500D7A4338D7A3676F039EA755E2",
+    "host"      =>"ASP-WebAppIngenieriaWeb-a1c2 (F1: Free)",
     "user"      =>"juansbkadminroot",
     "pass"      =>"Jzxc123asd456",
     "database"  =>"shopping_cart",
